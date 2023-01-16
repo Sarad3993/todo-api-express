14162c8c4986716dc3885022ba88367920c5f11b
Sarad3993 <risingstar3993@gmail.com>
2023-01-16 17:41:42 +0545

    changes in routes and controller

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,12 +1,33 @@
 
-
-
 const getAllTasks = (req,res)=>{
-    res.send('all items fetched')
+    res.send('get all tasks')
+}
+
+
+const createTask = (req,res)=>{
+    res.send('create tasks')
+}
+
+
+const getTask = (req,res)=>{
+    res.send('get single task')
+}
+
+
+const updateTask = (req,res)=>{
+    res.send('update task')
+}
+
+
+const deleteTask = (req,res)=>{
+    res.send('delete task')
 }
 
 
 
-
-
-module.exports = {getAllTasks}
+module.exports = {
+     getAllTasks ,
+     createTask, getTask, 
+     updateTask, 
+     deleteTask
+    }
--- routes/tasks.js
+++ routes/tasks.js
@@ -1,22 +1,19 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllTasks } = require('../controllers/tasks');
-
-
-
+const { getAllTasks, createTask, getTask, updateTask, deleteTask } = require('../controllers/tasks');
 
 
 //routes
 
 router.route('/api/v1/tasks')
-    .get(getAllTasks);
+    .get(getAllTasks)
+    .post(createTask)
 
-
-
-
-
-
+router.route('/api/v1/tasks/:id')
+    .get(getTask)
+    .patch(updateTask)
+    .delete(deleteTask)
 
 
 
