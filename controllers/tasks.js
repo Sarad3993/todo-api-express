076d8b0b90e540d5093fc9096f78c013c656259c
Sarad3993 <risingstar3993@gmail.com>
2023-01-18 22:51:06 +0545

    update and delete tasks

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -38,21 +38,44 @@ const getTask = async (req,res)=>{
 }
 
 
-const updateTask = (req,res)=>{
-    res.send('update task')
-}
+const updateTask = async (req,res)=>{
+    try {
+        const { id: taskID } = req.params;
+        const task = await Task.findOneAndUpdate({ _id: taskID },req.body,{
+            new:true,runValidators:true,
+        })
 
-
-const deleteTask = (req,res)=>{
-    res.send('delete task')
-}
-
-
-
-module.exports = {
-     getAllTasks ,
-     createTask,
-     getTask, 
-     updateTask, 
-     deleteTask
+        if(!task){
+             return res.status(404).json({ msg: `No task with id : ${taskID}`});
+        }
+        
+        res.status(200).json({msg:'Task updated',task})
+        
+    } catch (error) {
+        res.status(500).json({ msg: "Could not update the task" });
+        
     }
+}
+
+
+const deleteTask = async (req,res)=>{
+    try{
+        const { id: taskID } = req.params;
+        // destructuring is a convenient and shorthand way to extract properties from an object(here I used destructuring to extract ID from the array of objects in database and assigned it to taskID)
+        // But i can also simply do as 
+        // const taskID = req.params.id;
+        const task = await Task.findOneAndDelete({_id:taskID})
+        if(!task){
+            return res.status(404).json({msg:`No task with id : ${taskID}`})
+            } 
+        res.status(200).json({msg:`Task with id ${taskID} deleted`})
+        }
+        
+    catch (error) {
+        res.status(500).json({msg:'Could not delete the task'})
+    }
+}
+
+
+
+module.exports = {getAllTasks ,createTask,getTask, updateTask, deleteTask}
