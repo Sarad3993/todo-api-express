2291ecb227be4150aa36fa6b2e32db5863d4795a
Sarad3993 <risingstar3993@gmail.com>
2023-01-18 21:45:05 +0545

    controller for create and get tasks

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,17 +1,40 @@
 const Task = require('../models/Task')
 
-const getAllTasks = (req,res)=>{
-    res.send('get all tasks')
+const getAllTasks =async (req,res)=>{
+    try {
+        const tasks = await Task.find({})
+        res.status(200).json({msg:"All tasks fetched",tasks})
+    } catch (error) {
+        res.status(500).json({ msg: "Error fetching tasks"});
+    }
 }
 
 
 const createTask = async (req,res)=>{
-    const task = await Task.create(req.body)
-    res.status(201).json({task})
-}
+    try {
+        const task = await Task.create(req.body);
+        res.status(201).json({ task });
+        
+    } catch (error) {
+        res.status(500).json({msg:"Error creating task",error})
+    }
+} 
 
-const getTask = (req,res)=>{
-    res.json({id:req.params.id})
+
+const getTask = async (req,res)=>{
+    try {
+        const {id:taskID} = req.params
+        const task = await Task.findOne({_id:taskID})
+
+        if(!task){
+            return res.status(404).json({msg:`No task with id : ${taskID}`})
+            } 
+
+        res.status(200).json({msg: `Task associated with the id : ${taskID} fetched`,task});
+    }
+    catch (error) {
+        res.status(500).json({msg:'Could not fetch the task'})
+    }
 }
 
 
--- models/Task.js
+++ models/Task.js
@@ -2,8 +2,18 @@ const mongoose = require('mongoose');
 
 
 const TaskSchema = new mongoose.Schema({
-    name:String,
-    completed:Boolean
+    name:{
+        type:String,
+        required:[true,'Must provide name'],
+        trim:true,
+        maxlength:[20,'Name cannot be more than 20 characters']
+    },
+    completed:{
+        type:Boolean,
+        default:false
+    },
 })
+
+
 
 module.exports = mongoose.model('Task',TaskSchema)
